1a5b39527c0205bc79e139f14535ff79e3775765
imratatouille <130735410+imratatouille@users.noreply.github.com>
2023-11-24 04:18:46 +0900

    flask

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def student():
+   return render_template('index.html')
+
+@app.route('/posts')
+def response():
+    return{
+       "posts" : [
+           {
+               "id" : 11,
+               "title" : "게시글제목",
+               "author" : "게시글작성자이름"
+           }
+       ]
+   }
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
