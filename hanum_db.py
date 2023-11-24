5374779cd8b9d1d157e34d331e5d08e4307a7172
imratatouille <130735410+imratatouille@users.noreply.github.com>
2023-11-25 03:14:17 +0900

    db

--- hanum_db.py
+++ hanum_db.py
@@ -1,5 +1,4 @@
 import pymysql
-from flask import jsonify
 
 # db에 데이터 추가
 def insert_db(author, pwd, content, title):
@@ -12,7 +11,7 @@ def insert_db(author, pwd, content, title):
     db.commit()
     db.close()
     
-# db에 있는 post_table 내용 확인
+# db에 있는 post_table 중에 마지막 id내용 확인
 def idcheck():
     db = pymysql.connect(host="127.0.0.1", user="root", password="shivainu070309!", charset="utf8")
     cursor = db.cursor()
@@ -22,10 +21,51 @@ def idcheck():
     res = cursor.fetchone()
 
     for data in res:
-        data = data
-        # id = wow.replace("(","").replace(")","").replace(",","").replace("'","")
-        # id = int(id)
         return data
 
     db.commit() 
     db.close()
+    
+# db에 있는 post_table 에 있는 모든 내용 확인
+def check():
+    db = pymysql.connect(host="127.0.0.1", user="root", password="shivainu070309!", charset="utf8")
+    cursor = db.cursor()
+
+    cursor.execute('USE hanum_db;')
+    cursor.execute('select * from post_table')
+    
+    res = cursor.fetchall()
+
+    db.commit() 
+    db.close()
+    
+    return res
+
+# db에 있는 post_table 중에 마지막 id내용 확인
+def all_idcheck():
+    db = pymysql.connect(host="127.0.0.1", user="root", password="shivainu070309!", charset="utf8")
+    cursor = db.cursor()
+
+    cursor.execute('USE hanum_db;')
+    cursor.execute('select id from post_table')
+    res = cursor.fetchone()
+
+    for data in res:
+        return data
+
+    db.commit() 
+    db.close()
+
+def page(page):
+    db = pymysql.connect(host="127.0.0.1", user="root", password="shivainu070309!", charset="utf8")
+    cursor = db.cursor()
+
+    cursor.execute('USE hanum_db;')
+    cursor.execute(f'select * from post_table where id = {page}')
+    
+    res = cursor.fetchone()
+
+    db.commit() 
+    db.close()
+    
+    return res
