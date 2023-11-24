26a521bc4809bfec5062a5debf238da7c9701dee
imratatouille <130735410+imratatouille@users.noreply.github.com>
2023-11-25 03:14:10 +0900

    flask

--- app.py
+++ app.py
@@ -3,36 +3,21 @@ import hanum_db
 
 app = Flask(__name__)
 
-title = ""
-author = ""
-
-post = [
-    {
-            "id" : 1,
-            "title" : f"{title}",
-            "author" : f"{author}"
-        }
-]
+page = hanum_db.all_idcheck()
 
 @app.route('/')
 def student():
    return render_template('index.html')
 
-@app.route('/hello, world!')
-def hello():
-    if 1 == 1:
-        return("perpect")
-    else:
-        return("error")
-
 @app.route('/posts', methods=['GET','POST'])
 def postit():
     if request.method == 'GET':
-        return jsonify({"post": post})
+        post = hanum_db.check()
+        return jsonify({"post": post}) # db에 있는 내용을 json으로 보내는 과정에서 대괄호[]로 보내지는 현상 수정
     elif request.method == 'POST':
         posts = request.get_json()
         if len(posts) == 0:
-            return 'No post'
+            return 'No post' # 비었는데도 db에 올라가는 현상을 수정해야함
         else:
             author = posts['author']
             pwd = posts['password']
@@ -40,7 +25,11 @@ def postit():
             title = posts['title']
             hanum_db.insert_db(author=author, pwd=pwd, content=content, title=title)
             id = hanum_db.idcheck()
-            return jsonify({"id":id}) # post 보낼때 null값 나오는거 수정해야됨
+            return jsonify({"id":id}) # post 보낼때 null값 나오는거 수정해야됨 (수정완료)
+        
+@app.route(f'/posts/{page}')
+def mola():
+    return("hi this page is")
         
 if __name__ == '__main__':
    app.run(debug = True)
