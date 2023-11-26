e7703e8936ef9726fbf71271621bbf5b73e35953
imratatouille <130735410+imratatouille@users.noreply.github.com>
2023-11-27 03:44:15 +0900

    flask_app

--- app.py
+++ app.py
@@ -4,55 +4,60 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def student():
-   return render_template('index.html')
+@app.route('/posts', methods=['GET', 'POST'])
+def manage_posts():
+    if request.method == 'GET': # request.method가 get일때 limit, page의 args값을 받고 page랑 posts를 반환
+        limit = int(request.args.get('limit', 10))
+        page = int(request.args.get('page', 1))
+        posts = hanum_db.get_posts_page(page=page, limit=limit)
+        return jsonify({"pageCount": page, "posts": posts})
+    elif request.method == 'POST': # request.method가 post일때 json값을 받고 id부여하고 db에 저장
+        post_data = request.get_json()
+        author, pwd, content, title = post_data['author'], post_data['password'], post_data['content'], post_data['title']
+        now = datetime.now().isoformat()
+        if len(author) == 0:   # |
+            return 'No post'   # |
+        elif len(pwd) == 0:    # |
+            return 'No post'   # => author, pwd, content, title의 값이 0이면 'No post'를 return
+        elif len(content) == 0:# |
+            return 'No post'   # |
+        elif len(title) == 0:  # |
+            return 'No post'   # |
+        hanum_db.insert_post(author=author, pwd=pwd, content=content, title=title, datetime=now)
+        post_id = hanum_db.last_post_id()
+        return jsonify({"id": post_id})
 
-@app.route('/posts', methods=['GET','POST'])
-def postit():
-    if request.method == 'GET':
-        limit = request.args.get('limit')
-        limit = int(limit)
-        page = request.args.get('page')
-        page = int(page)
-        post = hanum_db.allcheck(limit=limit, page=page)
-        return jsonify({"pageCount":page,"post": post}) # db에 있는 내용을 json으로 보내는 과정에서 대괄호[]로 보내지는 현상 (수정)
-            
-    
-    elif request.method == 'POST': # post로 db에 값 넣기 기능 구현 완료
-        posts = request.get_json()
-        
-        if len(posts) == 0:
-            return 'No post' # 비었는데도 db에 올라가는 현상을 (수정)
+@app.route('/posts/<int:postID>', methods=['GET', 'DELETE']) # get => 고유postid값 받고 return
+def manage_post(postID):                                     # delete => 고유postid값 받고 pw값 비교후 삭제
+    if request.method == 'GET':                              # pw가 틀리면 false 맞아서 삭제되면 true 
+        post = hanum_db.get_post_by_id(postID)
+        return jsonify({"post": post})
+    elif request.method == 'DELETE':
+        get_pwd = request.args.get('password')
+        db_pwd = hanum_db.get_post_password(postID)
+        if get_pwd == db_pwd:
+            result = hanum_db.delete_post(postID)
+            return jsonify({"ok": result})
         else:
-            author = posts['author']
-            pwd = posts['password']
-            content = posts['content']
-            title = posts['title']
-            now = datetime.now()
-            now = now.isoformat()
-            hanum_db.insert_db(author=author, pwd=pwd, content=content, title=title, datetime=now)
-            id = hanum_db.last_id()
-            return jsonify({"id":id}) # post 보낼때 null값 나오는거 수정해야됨 (수정완료)
-    
-        
-@app.route('/posts/<postID>', methods=['GET']) # get으로 특정 postID 값 가져오기 기능 구현 완료
-def god(postID):
-        postID = int(postID)
-        pooost = hanum_db.check(postID)
-        return jsonify({"post":pooost})
-        
+            return jsonify({"ok": False})
 
-@app.route('/posts/<postID>', methods=['DELETE']) # delete 기능 구현 완료
-def delete(postID):
-    postID = int(postID)
+@app.route('/posts/<int:postID>/comments', methods=['POST']) # 고유 postid값 받고 comment하면 받은 postid에 comment가 달림
+def add_comment(postID):
+    comment_data = request.get_json()
+    author, pwd, content = comment_data['author'], comment_data['password'], comment_data['content']
+    hanum_db.insert_comment(author=author, pwd=pwd, content=content, postid=postID)
+    comment_id = hanum_db.last_comment_id()
+    return jsonify({"commentId": comment_id})
+
+@app.route('/posts/<int:postID>/comments/<int:commentID>', methods=['DELETE']) # db에 있는 comment-pw랑 받은 comment-pw값이 같으면 delete
+def delete_comment(postID, commentID):
     get_pwd = request.args.get('password')
-    db_pwd = hanum_db.check_pw(postID)
+    db_pwd = hanum_db.get_comment_password(commentID)
     if get_pwd == db_pwd:
-        hanum_db.del_db(postID) # db에서 postID가 진짜로 사라졌는 체크하고 ok 보내기 (수정)
-        return jsonify({"ok":True})
+        result = hanum_db.delete_comment(commentID)
+        return jsonify({"ok": result})
     else:
-        return jsonify({"ok":False})
+        return jsonify({"ok": False})
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
