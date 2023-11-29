544f853398df7920f322920b7ea23b45dc8aeff8
imratatouille <130735410+imratatouille@users.noreply.github.com>
2023-11-29 23:57:37 +0900

    mysql_query
    최종완성

--- hanum_database.sql
+++ hanum_database.sql
@@ -5,17 +5,49 @@ CREATE TABLE posts_table (
     password VARCHAR(45) NOT NULL,
     content VARCHAR(45) NOT NULL,
     title VARCHAR(45) NOT NULL,
-    datetime VARCHAR(45) NOT NULL,
+    comment INT DEFAULT 0,
+    uploadAt VARCHAR(45) NOT NULL,
     PRIMARY KEY (id),
     UNIQUE KEY id_UNIQUE (id)
 );
 -- comments_table create query
 CREATE TABLE comments_table (
-    comment_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author VARCHAR(45) NOT NULL,
     password VARCHAR(45) NOT NULL,
     content VARCHAR(45) NOT NULL,
     post_id INT,
-    PRIMARY KEY (comment_id),
-    UNIQUE KEY id_UNIQUE (comment_id)
+    uploadAt VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY id_UNIQUE (id)
 );
+
+-- trigger + 
+DELIMITER //
+
+CREATE TRIGGER update_comments_count
+AFTER INSERT ON comments_table
+FOR EACH ROW
+BEGIN
+    UPDATE posts_table
+    SET comment = comment + 1
+    WHERE id = NEW.post_id;
+END;
+//
+
+DELIMITER ;
+
+-- trigger -
+DELIMITER //
+
+CREATE TRIGGER decrease_comments_count
+AFTER DELETE ON comments_table
+FOR EACH ROW
+BEGIN
+    UPDATE posts_table
+    SET comment = comment - 1
+    WHERE id = OLD.post_id;
+END;
+//
+
+DELIMITER ;
